69967c37d490ac1b686c67728d61924d601651e6
Marouen Helali <marwanest@live.fr>
2023-09-26 20:53:39 +0100

    add forge api

--- openforge/src/index.jsx
+++ openforge/src/index.jsx
@@ -1,6 +1,21 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
+import api, { route } from "@forge/api";
+import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState } from "@forge/ui";
+
+const fetchCommentsForIssue = async (issueIdOrKey) => {
+  const res = await api
+    .asUser()
+    .requestJira(route`/rest/api/3/issue/${issueIdOrKey}/comment`);
+
+  const data = await res.json();
+  return data.comments;
+};
 
 const App = () => {
+  const context = useProductContext();
+  const [comments] = useState(async () => await fetchCommentsForIssue(context.platformContext.issueKey));
+
+  console.log(`Number of comments on this issue: ${comments.length}`);
+
   return (
     <Fragment>
       <Text>Hello world!</Text>
